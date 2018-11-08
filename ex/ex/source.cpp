8632c6c22064fa0b89c096150d561dc8f7f2a758
IOTnewb <42164398+IOTnewb@users.noreply.github.com>
2018-11-08 12:19:59 +0900

    문자열

--- ex/ex/source.cpp
+++ ex/ex/source.cpp
@@ -2,12 +2,37 @@
 
 int main(void)
 {
+	/*
+	char name[100];
+	char lang[200];
+	
+	std::cout << "이름을 입력하세요 : ";
+	std::cin >> name;
+
+	std::cout << "좋아하는 언어 : ";
+	std::cin >> lang;
+
+	std::cout << "내 이름은 " << name << "입니다. \n" << std::endl;
+	std::cout << "좋아하는 언어는 " << lang << "입니다." << std::endl;
+
+	
+	int val;
+	std::cout << "첫 번째 숫자를 입력 : ";
+	std::cin >> val;
+	
+	int val2;
+	std::cout << "두 번째 숫자를 입력 : ";
+	std::cin >> val2;
+
+	int result = val + val2;
+	std::cout << "덧셈 결과 : " << result << std::endl;
+
 
 	int num = 20;
 	std::cout << "Hello World." << std::endl;
 	std::cout << "hello" << "world." << std::endl;
 	std::cout << num << ' ' << 'A';
 	std::cout << ' ' << 3.14 << std::endl;
-
+	*/
 	return 0;
 }
