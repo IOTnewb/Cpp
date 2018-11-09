c7043ef5f8319e73dc6dfca211844c999bde2b46
IOTnewb <42164398+IOTnewb@users.noreply.github.com>
2018-11-09 11:04:22 +0900

    참조자_

--- ex/ex_1109/study.cpp
+++ ex/ex_1109/study.cpp
@@ -2,13 +2,59 @@
 
 using namespace std;
 
+void SwapByRef(int &ref1, int &ref2)
+{
+	int temp = ref1;
+	ref1 = ref2;
+	ref2 = temp;
+}
+
+int& RefRetFuncOne(int &ref)
+{
+	ref++;
+	return ref;
+}
+
 int main(void)
 {
-	//포인터 변수를 대상으로 한 참조자
+	// 반환형이 참조자인 경우
+	int num2 = 1;
+	int &num3 = RefRetFuncOne(num2);
+	// int &num3 = num2;
+	num2++;
+	num3++;
+
+	cout << "num2 :" << num2 << endl;
+	cout << "num3 :" << num3 << endl;
+
+
+	/* 참조자 이용한 call by reference
+	int val2 = 10;
+	int val3 = 20;
+
+	SwapByRef(val2, val3);
+
+	cout << "val2 : " << val2 << endl;
+	cout << "val3 : " << val3 << endl;
+	
+	/*
+	call by value - 값을 인자로 전달하는 함수 호출방식
+	call by reference - 주소 값을 인자로 전달하는 함수 호출방식
+	
+
+	/* 포인터 변수를 대상으로 한 참조자
 
 	int num = 12;
 	int *ptr = &num;
 	int **dptr = &ptr;
+
+	int &ref = num;
+	int *(&pref) = ptr;
+	int **(&dpref) = dptr;
+
+	cout << ref << endl;
+	cout << *pref << endl;
+	cout << **dpref << endl;
 
 
 	/* 배열요소 대상 참조자
