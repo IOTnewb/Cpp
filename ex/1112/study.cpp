4744d92a68e6397e540b151ee34af47bc2af024a
IOTnewb <42164398+IOTnewb@users.noreply.github.com>
2018-11-12 10:18:55 +0900

    1_객체생성알고리즘 작성

---
+++ ex/1112/study.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+/*
+	- 나는 과일장수에게 5000원어치 사과 구매한다.
+	- obj 후보
+		과일장수, 과일구매자
+
+
+*/
+
+class FruitSeller
+{
+	// 단가
+	// 재고
+	// 판매 수익
+
+	// 멤버 변수 초기화
+	// 사과 판매
+	// 현재 상태 확인
+};
+
+class FruitBuyer
+{
+	// 잔고
+	// 사과 수량
+
+	// 멤버 변수 뭐시기
+	// 사과 구입
+	// 현재 내 상태
+};
+
+int main(void)
+{
+	return 0;
+}
