2156b6bc1e07925e67603c26bea9390a46b98538
IOTnewb <42164398+IOTnewb@users.noreply.github.com>
2018-11-20 14:13:23 +0900

    예외처리

--- ex/1120/Study.cpp
+++ ex/1120/Study.cpp
@@ -2,71 +2,40 @@
 #include <cstring>
 
 using namespace std;
-/*		»ó¼Ó °ü°è¿¡¼­ÀÇ ´ëÀÔ ¿¬»êÀÚ
-		class A					|		class B
-		int num1 num2		int num3 num4
-		class B : public A
-		- ºÎ¸ğ class ´ëÀÔ ¿¬»êÀÚ°¡ ÀÚµ¿À¸·Î ºÒ·¯Áö³ª?
-*/
-/*	 »ó¼Ó °ü°è¿¡¼­ ±âº» ´ëÀÔ ¿¬»êÀÚ È£Ãâ */
-
-class First
-{
-private:
-	int num1;
-	int num2;
-public:
-	First(int n1=0, int n2=0)
-		: num1(n1), num2(n2)
-	{}
-	void ShowData()
-	{
-		cout << "[" << num1 << ", " << num2 << "]" << endl;
-	}
-	
-	First &operator=(const First &cpy)
-	{
-		cout << "First &operator=()" << endl;
-		num1 = cpy.num1;
-		num2 = cpy.num2;
-		return *this;
-	}
-};
-
-class Second	: public First
-{
-private:
-	int num3;
-	int num4;
-public:
-	Second(int n1 = 0, int n2 = 0, int n3 = 0, int n4 = 0)
-		: First(n1, n2), num3(n3), num4(n4)
-	{}
-	void ShowData()
-	{
-		First::ShowData();
-		cout << "[" << num3 << ", " << num4 << "]" << endl;
-	}
-	/*		´ëÀÔ ¿¬»êÀÚ ¿À¹ö·Îµù	
-			- ºÎ¸ğ, ÀÚ½Ä°£ µ¿ÀÏÇÑ ´ëÀÔ¿¬»êÀÚ ¿À¹ö·ÎµùÀÌ ÀÖÀ¸¸é
-			- ÀÚ½ÄÀÇ ´ëÀÔ¿¬»êÀÚ ¿À¹ö·Îµù¸¸ È£Ãâ
-			- ÀÚ½Ä ´ëÀÔ¿¬»êÀÚ ¿À¹ö·Îµù ¾È¿¡ ºÎ¸ğ ´ëÀÔ¿¬»êÀÚ ¿À¹ö·Îµù ÇÔ¼ö È£Ãâ
-	*/
-	Second &operator=(const Second &cpy)
-	{
-		First::operator=(cpy);		// ´ÙÇü¼º (»ó¼Ó°ü°è°¡ is A °ü°è)
-		cout << "Second &operator=()" << endl;
-		num3 = cpy.num3;
-		num4 = cpy.num4;
-
-		return *this;
-	}
-};
 
 
 
+//	class Second	: public First
+//{
+//private:
+//	int num3;
+//	int num4;
+//public:
+//	Second(int n1 = 0, int n2 = 0, int n3 = 0, int n4 = 0)
+//		: First(n1, n2), num3(n3), num4(n4)
+//	{}
+//	void ShowData()
+//	{
+//		First::ShowData();
+//		cout << "[" << num3 << ", " << num4 << "]" << endl;
+//	}
+//	/*		´ëÀÔ ¿¬»êÀÚ ¿À¹ö·Îµù	
+//			- ºÎ¸ğ, ÀÚ½Ä°£ µ¿ÀÏÇÑ ´ëÀÔ¿¬»êÀÚ ¿À¹ö·ÎµùÀÌ ÀÖÀ¸¸é
+//			- ÀÚ½ÄÀÇ ´ëÀÔ¿¬»êÀÚ ¿À¹ö·Îµù¸¸ È£Ãâ
+//			- ÀÚ½Ä ´ëÀÔ¿¬»êÀÚ ¿À¹ö·Îµù ¾È¿¡ ºÎ¸ğ ´ëÀÔ¿¬»êÀÚ ¿À¹ö·Îµù ÇÔ¼ö È£Ãâ
+//	*/
+//	Second &operator=(const Second &cpy)
+//	{
+//		First::operator=(cpy);		// ´ÙÇü¼º (»ó¼Ó°ü°è°¡ is A °ü°è)
+//		cout << "Second &operator=()" << endl;
+//		num3 = cpy.num3;
+//		num4 = cpy.num4;
+//
+//		return *this;
+//	}
+//};
 
-/* µğÆúÆ® ´ëÀÔ ¿¬»êÀÚÀÇ ¹®Á¦Á¡ */
+ //µğÆúÆ® ´ëÀÔ ¿¬»êÀÚÀÇ ¹®Á¦Á¡ 
 //class Person
 //{
 //private:
@@ -103,23 +72,23 @@ public:
 //		cout << "called Destruct" << endl;
 //	}
 //};
-//
-//
-//class First
-//{
-//private:
-//	int num1;
-//	int num2;
-//public:
-//	First(int n1=0, int n2=0)
-//		: num1(n1), num2(n2)
-//	{}
-//	void ShowData()
-//	{
-//		cout << "[" << num1 << ", " << num2 << "]" << endl;
-//	}
-//};
-//
+
+
+/*class First
+{
+private:
+	int num1;
+	int num2;
+public:
+	First(int n1=0, int n2=0)
+		: num1(n1), num2(n2)
+	{}
+	void ShowData()
+	{
+		cout << "[" << num1 << ", " << num2 << "]" << endl;
+	}
+};*/
+
 //class Second
 //{
 //private:
@@ -139,42 +108,127 @@ public:
 //		cout << "Second& operator=()" << endl;
 //		num3 = cpy.num3;
 //		num4 = cpy.num4;
-//
 //		return *this;
 //	}
 //};
 
+/*		»ó¼Ó °ü°è¿¡¼­ÀÇ ´ëÀÔ ¿¬»êÀÚ
+class A					|		class B
+int num1 num2		int num3 num4
+class B : public A
+- ºÎ¸ğ class ´ëÀÔ ¿¬»êÀÚ°¡ ÀÚµ¿À¸·Î ºÒ·¯Áö³ª?
+*/
+
+/*	 »ó¼Ó °ü°è¿¡¼­ ±âº» ´ëÀÔ ¿¬»êÀÚ È£Ãâ */
+/*class First
+{
+private:
+int num1;
+int num2;
+public:
+First(int n1=0, int n2=0)
+: num1(n1), num2(n2)
+{}
+void ShowData()
+{
+cout << "[" << num1 << ", " << num2 << "]" << endl;
+}
+
+First &operator=(const First &cpy)
+{
+cout << "First &operator=()" << endl;
+num1 = cpy.num1;
+num2 = cpy.num2;
+return *this;
+}
+};*/
+
+	/* ¹è¿­ ¼±¾ğ ¿¬»êÀÚ ¿À¹ö·Îµù*/
+//class BoundCheckIntArray
+//{
+//private:
+//	int *arr;
+//	int arrlen;
+//public:
+//	BoundCheckIntArray(int len)
+//		: arrlen(len)
+//	{
+//		arr = new int[len];
+//	}
+//
+//	int Getlen() const
+//	{
+//		return arrlen;
+//	}
+//
+//	int &operator[](int idx) const
+//	{
+//		// ¹è¿­ ¹üÀ§ Ã¼Å©
+//		if (idx < 0 || idx >= arrlen)
+//		{
+//			cout << "¹è¿­ ¹üÀ§¸¦ ¹ş¾î³µ¤¨" << endl;
+//			exit(1);
+//		}
+//
+//		return arr[idx];
+//	}
+//
+//	int &operator[](int idx)
+//	{
+//		// ¹è¿­ ¹üÀ§ Ã¼Å©
+//		if (idx < 0 || idx >= arrlen)
+//		{
+//			cout << "¹è¿­ ¹üÀ§¸¦ ¹ş¾î³µ¤¨" << endl;
+//			exit(1);
+//		}
+//
+//		return arr[idx];
+//	}
+//
+//	~BoundCheckIntArray()
+//	{
+//		delete[]arr;
+//	}
+//};
+//
+//void ShowAllData(const BoundCheckIntArray &cpy)
+//{
+//	for (int i = 0; i < cpy.Getlen(); i++)
+//	{
+//		cout << cpy[i] << endl;
+//	}
+//}
+
+/* ¿¹¿Ü Ã³¸® - Exception Handling */
 
 
 int main(void)
 {
 	
-	/*		¾ÆÄ§ ¼ö¾÷
-			1. ´ëÀÔ ¿¬»êÀÚÀÇ ¿À¹ö·Îµù
-			- º¹»ç »ı¼ºÀÚ¿Í ºñ½Á
-			Ex) º¹»ç »ı¼ºÀÚ
-			Point pos1(3, 5);
-			Point pos2 = pos1;		<- º¹»ç »ı¼ºÀÚ	
-			(= Point pos2(pos1);) <- ÄÄÆÄÀÏ·¯°¡ ÄÚµå º¯°æ
-			´ÜÁ¡ = ¾èÀº º¹»ç°¡ ÀÏ¾î³­´Ù.
-			¾èÀºº¹»ç : ¸â¹ö ´ë ¸â¹ö º¹»ç -> ±íÀºº¹»ç
+/*		¾ÆÄ§ ¼ö¾÷
+		1. ´ëÀÔ ¿¬»êÀÚÀÇ ¿À¹ö·Îµù
+		- º¹»ç »ı¼ºÀÚ¿Í ºñ½Á
+		Ex) º¹»ç »ı¼ºÀÚ
+		Point pos1(3, 5);
+		Point pos2 = pos1;		<- º¹»ç »ı¼ºÀÚ	
+		(= Point pos2(pos1);) <- ÄÄÆÄÀÏ·¯°¡ ÄÚµå º¯°æ
+		´ÜÁ¡ = ¾èÀº º¹»ç°¡ ÀÏ¾î³­´Ù.
+		¾èÀºº¹»ç : ¸â¹ö ´ë ¸â¹ö º¹»ç -> ±íÀºº¹»ç
 	
 
-	 point pos1(5, 7);
-	 point pos2(9, 10);
-	 pos2 = pos1; -> pos2.operator=(pos1);
-	 ÀÌ °æ¿ì ¿¬»êÀÚ ¿À¹ö·ÎµùÀÌ ÇÊ¿äÇÏ´Ù.
+		point pos1(5, 7);
+		point pos2(9, 10);
+		pos2 = pos1; -> pos2.operator=(pos1);
+		ÀÌ °æ¿ì ¿¬»êÀÚ ¿À¹ö·ÎµùÀÌ ÇÊ¿äÇÏ´Ù.
 	*/
+
 	//First first(111, 222);
 	//First cpy;
-
 	//Second second(333, 444);
 	//Second secpy;
-
 	//cpy = first;
 	//cpy.ShowData();
 	//first.ShowData();
-
 	//secpy = second;				// ´ëÀÔ ¿¬»êÀÚ ¿À¹ö·Îµù È£Ãâ
 	//second.ShowData();
 	//secpy.ShowData();
@@ -194,13 +248,76 @@ int main(void)
 	//man2.ShowPerson();
 
 	/*	 »ó¼Ó °ü°è¿¡¼­ ±âº» ´ëÀÔ ¿¬»êÀÚ È£Ãâ */
-	Second second(111, 222, 333, 444);
+	/*Second second(111, 222, 333, 444);
 	Second secpy;
 
 	secpy = second;
 
 	secpy.ShowData();
 	second.ShowData();
+	*/
 
+	/* ¹è¿­ ÀÎµ¦½º ¿¬»êÀÚ ¿À¹ö·Îµù */
+
+	//arrobject[2];
+
+	//complier view
+	//arrobject.operator[](2)
+
+	//programmer view
+	/*BoundCheckIntArray arr(5);
+	for (int i = 0; i < 5; i++)
+	{
+		arr[i] = (i + 1) * 11;
+	}*/
+	
+	/*for (int i = 0; i < 6; i++)
+	{
+		cout << arr[i] << endl;
+	}*/
+	
+	//ShowAllData(arr);
+
+	/* ¿¹¿Ü Ã³¸® - Exception Handling in C++ */
+	//try ºí·Ï - ¿¹ ¿Ü ¹ß °ß
+	//catch ºí·Ï - ¿¹ ¿Ü Ã³ ¸®
+	//throw ºí·Ï - ¿¹ ¿Ü Ã³ ¸® À§ ÀÓ
+	int num1, num2;
+	cout << "µÎ °³ÀÇ ¼ıÀÚ ÀÔ·Â : ";
+	cin >> num1 >> num2;		// ¿¹ ¿Ü ¹ß »ı
+
+	try
+	{
+		if (num2 == 0)					// ¿¹ ¿Ü ®c
+			throw num2;
+		else
+		{
+			cout << "³ª´°¼ÀÀÇ ¸ò : " << num1 / num2 << endl;
+			cout << "³ª´°¼ÀÀÇ ³ª¸ÓÁö : " << num1 % num2 << endl;
+		}
+	}
+	catch(int expn)			// try¿¡¼­ throw µÈ µ¥ÀÌÅÍ Å¸ÀÔÀ¸·Î ¹Ş¾Æ¾ßµÈ´Ù.
+	{
+		cout << "Á¦¼ö´Â " << expn <<"ÀÌ µÉ ¼ö ¾ø½À´Ï´Ù." << endl;
+		cout << "ÇÁ·Î±×·¥ ¸®½ºÅ¸¶Ç" << endl;
+	}
+	
+
+	/* ¿¹¿Ü Ã³¸® - Exception Handling in C */
+	//int num1, num2;
+	//cout << "µÎ °³ÀÇ ¼ıÀÚ ÀÔ·Â : ";
+	//cin >> num1 >> num2;		// ¿¹ ¿Ü ¹ß »ı
+	//
+	//if (num2 == 0)					// ¿¹ ¿Ü ®c
+	//{												// ¿¹ ¿Ü Ã³ ¸®
+	//	cout << "Á¦¼ö´Â 0ÀÌ µÉ ¼ö ¾ø½À´Ï´Ù." << endl;
+	//	cout << "ÇÁ·Î±×·¥ ¸®½ºÅ¸¶Ç" << endl;
+	//}
+	//else
+	//{
+	//	cout << "³ª´°¼ÀÀÇ ¸ò : " << num1 / num2 << endl;
+	//	cout << "³ª´°¼ÀÀÇ ³ª¸ÓÁö : " << num1 % num2 << endl;
+
+	//}
 	return 0;
 }
